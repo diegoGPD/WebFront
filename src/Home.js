899ad8e52768e1cb3061d88ae2fb0ecaf8efd039
chuca1 <diego.gm99@hotmail.com>
2022-11-26 15:15:30 -0600

    a ver

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {Button, CardActions, Container, CssBaseline, Grid, Typography} from "@mui/material";
 import {AppBar, Card, Toolbar} from "@mui/material";
+import Chat from "./chat/Chat";
 //import { StrictMode } from "react";
 //import ReactDOM from "react-dom/client";
-const cards = [1, 1, 1, 1, 1, 1, 1];
+const cards = [{key:1, name: "John"},{key:2, name: "Mark"},{key:3, name: "Joe"},{key:4, name: "Griffin"},{key:5, name: "Timmy"}];
+let cliente = false;
 
 export default function Home() {
+    const [mostrarChat, setMostrarChat] = useState(false);
+    const [cardClinete, setCardClinete] = useState([]);
+    const [buttonClick, setButtonClick] = useState(false);
 
+    const necesitoAyuda = () =>{
+        setCardClinete([{key:1, name: "John"}]) 
+        setButtonClick(true);
+    }
     return (
         <React.Fragment>
             <CssBaseline />
@@ -17,53 +26,82 @@ export default function Home() {
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <main>
-                <div >
-                    <Container maxWidth="sm">
-                        <Typography
-                            component="h1"
-                            variant="h2"
-                            align="center"
-                            color="textPrimary"
-                            gutterBottom
-                        >
-                            Clientes disponibles
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            align="center"
-                            color="textSecondary"
-                            paragraph
-                        >
-                            Estos son los clientes a quienes no se les ha asignado soporte
-                        </Typography>
-
+            <div style={{display: "flex", flexDirection: "row"}}>
+                <main>
+                    <div >
+                        <Container maxWidth="sm">
+                            <Typography
+                                component="h1"
+                                variant="h2"
+                                align="center"
+                                color="textPrimary"
+                                gutterBottom
+                            >
+                                {cliente ? 'Mi Plataforma' : `Clientes disponibles`}
+                            </Typography>
+                            {!cliente && 
+                                <Typography
+                                    variant="h5"
+                                    align="center"
+                                    color="textSecondary"
+                                    paragraph
+                                >
+                                    Estos son los clientes a quienes no se les ha asignado soporte
+                                </Typography>
+                            }
+                        </Container>
+                    </div>
+                    <Container maxWidth="md">
+                        {cliente && 
+                        <div>
+                            {buttonClick && 
+                                <Button type='submit' size="small" variant='contained' color="secondary" onClick={necesitoAyuda}>
+                                    Necesito ayuda
+                                </Button>
+                            }
+                            {cardClinete.map((card) => (
+                            <Grid item key={card.key} xs={12} sm={6} md={4}>
+                            <Card >
+                                <cardContent >
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {`Recibe un asistente aqui`}
+                                    </Typography>
+                                    <Typography>
+                                       Manda un mensaje de whatsapp a este numero +528711840155
+                                    </Typography>
+                                </cardContent>
+                            </Card>
+                        </Grid>
+                            ))}
+                        </div>
+                        }
+                        <Grid container spacing={4}>
+                            {!cliente && cards.map((card) => (
+                                <Grid item key={card.key} xs={12} sm={6} md={4}>
+                                    <Card >
+                                        <cardContent >
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {`Cliente ${card.name}`}
+                                            </Typography>
+                                            <Typography>
+                                                Descripcion del cliente
+                                            </Typography>
+                                        </cardContent>
+                                        <CardActions>
+                                            
+                                            <Button type='submit' size="small" variant='contained' color="primary" onClick={() => setMostrarChat(true)}>
+                                                Asistir
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
                     </Container>
-                </div>
-                <Container maxWidth="md">
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card >
-                                    <cardContent >
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Cliente
-                                        </Typography>
-                                        <Typography>
-                                            Descripcion del cliente
-                                        </Typography>
-                                    </cardContent>
-                                    <CardActions>
-                                        <Button type='submit' size="small" variant='contained' color="primary">
-                                            Asistir
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </main>
+                </main>
+                {mostrarChat && <Chat/>
+                }
+            </div>
         </React.Fragment>
     );
 }
---
+++ src/chat/Chat.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import "./ChatStyle.css";
+
+import ChatWindow from "./ChatWindow";
+import ChatComposer from "./ChatComposer";
+
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [
+        { text: "Disculpa me puedes ayudar" },
+      ]
+    };
+  }
+
+  // if new message was submitted from child component // process
+  submitted = getNewMessage => {
+    if (getNewMessage != "") {
+      // match the state format
+      const newMessage = { text: getNewMessage };
+      // merge new message in copy of state stored messages
+      let updatedMessages = [...this.state.messages, newMessage];
+      // update state
+      this.setState({
+        messages: updatedMessages
+      });
+
+      setTimeout(async() => {
+        try {
+            const message = await axios.get(`https://web-backend-tec.herokuapp.com/checklogin?email=bob2@mail.com&password=bob`,[]);
+            const message2 = await axios.get(`https://web-backend-tec.herokuapp.com/whatsapp`,[]);
+            console.log(message2);
+            let updatedMessages = [...this.state.messages, {text:"HIHIHA SOY FALSO"}];
+            this.setState({
+                messages: updatedMessages
+            });
+            console.log(message)
+
+        } catch (e) {
+            console.log(e)
+        }
+        let updatedMessage2 = [...this.state.messages, {text:"HIHIHA SOY FALSO"}];
+        // update state
+        this.setState({
+            messages: updatedMessages2
+        });
+      }, 2000)
+    }
+  };
+
+
+  render() {
+    return (
+      <div className="Chat">
+        <h1>Chat de ayuda</h1>
+        {/* send stored messages as props to chat window */}
+        <ChatWindow messagesList={this.state.messages} />
+        {/* send submitted props to chat composer */}
+        <ChatComposer submitted={this.submitted} />
+      </div>
+    );
+  }
+}
